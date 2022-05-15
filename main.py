8117f6acda1f6bf90a01d9b1acd58ee6e4d1daa8
Yahia Salaheldin Shaaban <62369984+yehias21@users.noreply.github.com>
2022-05-15 19:06:33 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pickle
-if __name__ == '__main__':
-    configDic={}
-    try:
-        with open('cache/clientdict.config', 'rb') as config_dictionary_file:
-            configDic = pickle.load(config_dictionary_file)
-    except:
-        pass
-    print(configDic)
